ae3c132059940deadad3afd3a54350ae004f9f58
Pedro Rosa <pedrorosadomingues@gmail.com>
2024-06-19 21:17:23 -0300

    Adjust initial rotation of CAPA image in ThreeSixtyViewer

--- src/pages/ThreeSixtyViewer.js
+++ src/pages/ThreeSixtyViewer.js
@@ -43,6 +43,9 @@ const ThreeSixtyViewer = () => {
       const material = new THREE.MeshBasicMaterial({ map: texture });
       const mesh = new THREE.Mesh(geometry, material);
 
+      // Ajustar a rotação inicial da imagem CAPA 
+      mesh.rotation.y = THREE.MathUtils.degToRad(-50);
+
       scene.add(mesh);
     });
 
